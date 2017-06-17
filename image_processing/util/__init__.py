20e150510a812f4e8fb5c1f167b4401e6147017b
Daniel Merz <MerzDanielDeveloper@gmail.com>
2017-06-17 11:37:05 +0200

    [linalg] closest_point search with normalized_step_length

--- image_processing/util/__init__.py
+++ image_processing/util/__init__.py
@@ -12,14 +12,19 @@ class Vector3D:
         self.start_point = numpy.array(start_point)
         self.vector = numpy.array(vector)
 
+    def length(self):
+        return distance(self.vector, [0] * len(self.vector))
+
     def closest_point(self, target_point):
         min_step_size = 0.0001
+        normalized_min_step_size = min_step_size / self.length()
+
         def calc_next_point(X, step_size):
             return X + (self.vector * step_size)
         # iterative step by step of shortest distance
         def recursive_search(target, X, step_size):
             # todo newton method for speed up
-            if math.fabs(step_size ) < min_step_size: return X    # step size has reached minimum
+            if math.fabs(step_size ) < normalized_min_step_size: return X    # step size has reached minimum
             curr_distance = distance(target, X)
             if curr_distance == 0: return X # directly hit target point
             curr_point = X
