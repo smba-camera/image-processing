31bfd3c304e4a0bbc5ebe4ac95e17b5c4be24300
Daniel Merz <MerzDanielDeveloper@gmail.com>
2017-06-17 12:03:27 +0200

    [objectDetection] package for object detection

---
+++ image_processing/object_detection/object_detection.py
@@ -0,0 +1,13 @@
+
+def find_objects(img):
+    objects = []
+    return objects
+
+class Object:
+    def __init__(self, X=0, Y=0, width=0, height=0, type=""):
+        self.X = X
+        self.Y = Y
+        self.width = width
+        self.height = height
+        self.type = type
+
