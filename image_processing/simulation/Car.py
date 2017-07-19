db1d06a1d3f8c288ada5688a0c735e17a13d121c
Daniel Merz <MerzDanielDeveloper@gmail.com>
2017-07-19 15:55:53 +0200

    remove image from Car data structure

--- image_processing/simulation/Car.py
+++ image_processing/simulation/Car.py
@@ -2,8 +2,6 @@ from Image import *
 
 class Car:
 
-    current_view = Image(640,480)
-
     #x and y are reflected as the center of the car the car grows 4.7/2 and 1./9 in the respective directions
     def __init__(self,x=2,y=4,length=4.7,width=1.9,theta=90):
         print("Car created")
@@ -12,11 +10,9 @@ class Car:
         self.length = length
         self.width = width
         self.theta = theta #is the angle
-        self.current_view = Image(640,480) #set the start image
 
     def moveCar(self,new_x,new_y,new_theta):
         self.x_coor = new_x
         self.y_coor = new_y
         self.theta = new_theta
-        self.current_view = Image(640,480) #update the new image
 
