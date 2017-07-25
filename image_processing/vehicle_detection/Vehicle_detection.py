ca61c13a5afaae405507026307e7c70c8d6cd3e8
Daniel Merz <MerzDanielDeveloper@gmail.com>
2017-07-25 18:08:16 +0200

    [VehicleDetection] load linux specific dependencies only when needed

--- image_processing/vehicle_detection/Vehicle_detection.py
+++ image_processing/vehicle_detection/Vehicle_detection.py
@@ -1,4 +1,4 @@
-from vehicle_detection_svm import init, find_vehicles,show_vehicles,predict64by64image
+
 import os,glob
 
 
@@ -6,10 +6,14 @@ class VehicleDetection:
     def __init__(self, img):
         # color image is needed
         height, width, channels = img.shape
+        from vehicle_detection_svm import init
         init(os.path.join('..','image_processing','vehicle_detection','svm_model.pkl'),os.path.join('..','image_processing','vehicle_detection','scalar.pkl'), height, width)
     def find_vehicles(self, img):
+        from vehicle_detection_svm import find_vehicles
         return find_vehicles(img)
     def show_vehicles(self, img):
+        from vehicle_detection_svm import show_vehicles
         show_vehicles(img)
     def predict(selfself,img):
+        from vehicle_detection_svm import predict64by64image
         return predict64by64image(img)
