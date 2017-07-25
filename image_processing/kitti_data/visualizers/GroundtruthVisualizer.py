adea15260efb352164f3a937b63ffcd5b001c772
Daniel Merz <MerzDanielDeveloper@gmail.com>
2017-07-25 18:09:38 +0200

    [GroundTruthVisualizer] with range now

--- image_processing/kitti_data/visualizers/GroundtruthVisualizer.py
+++ image_processing/kitti_data/visualizers/GroundtruthVisualizer.py
@@ -1,20 +1,21 @@
-import glob,os
-import matplotlib.image
+import os
+import time
 import matplotlib.pyplot as plt
-import cv2
-import math
-import random
 import matplotlib.patches as patches
 from ..vehicle_positions import VehiclePositions
+import cv2
+import sys
+import image_processing.util.Util as util
 
-# Visualizes the real position of the cars
+
+# detected cars within the kitti images and estimations for distances
 
 class GroundtruthVisualizer:
     def __init__(self, kitti, drive_num):
-        self.camera_model = kitti.getVeloCameraModel()
+        self.camera_model_velo_camera_0 = kitti.getVeloCameraModel()
+        self.camera_model_1 = kitti.getCameraModel(0)
+        self.camera_model_2 = kitti.getCameraModel(1)
         self.drive_num = drive_num
-        self.car_count=0
-        self.Nocar_count=0
 
     def getVehicleColor(self, name):
         color='none'
@@ -36,49 +37,22 @@ class GroundtruthVisualizer:
             color = 'grey'
         return color
 
-    def save_non_cars(self,img,list):
-        height,width,_=img.shape
-        xstart=random.randint(0,500)
-        ystart=random.randint(0,200)
-        for x in range(xstart,width-64,5*96):
-            for y in range(ystart,height-64,3*96):
-                nocar=True
-                for pos in list:
-                    if not((x<pos[0]-50) or (x>pos[0]+100)) and ((y<pos[1]+50) or (y>pos[1]-100)):
-                        nocar=False
-                if nocar:
-                    scale=random.uniform(1,2.5)
-                    im=img[int(y):int(y+(64/scale)),int(x):int(x+(64/scale))]
-                    im = cv2.resize(im, (64, 64))
-                    cv2.imwrite('non_vehicles/'+str(self.drive_num)+'Nonvehicle'+str(self.Nocar_count)+'.png',im)
-                    self.Nocar_count+=1
+    def getRealVehiclePositions(self, imgId, vehiclePositions):
+        ''' finds vehicles within the image. Will just '''
+        return vehiclePositions.getVehiclePosition(imgId)
 
-    def checkCoord(self,img,pos):
-        height,width,_=img.shape
-        if pos[0]<0:
-            pos[0]=0
-        if pos[0]>width-64:
-            pos[0]=width-64
-        if pos[1]-64<0:
-            pos[1]=64
-        if pos[1]>height:
-            pos[1]=height
-        return pos[0],pos[1]
+    def findVehiclesOnImages(self, img1, img2, realVehiclePositions=None):
+        '''Returns a list of tuples (RectancleOnImg1, RectanleOnImg2)'''
+        img_coords = []
 
+        # fake implementation for the time the implementation of the detection of vehicles is not finished
+        for v in realVehiclePositions:
+            img_coord_0 = self.camera_model_velo_camera_0.projectToImage([v.xPos, v.yPos, v.zPos])
+            # TODO: next line is currently wrong (projection to image0)
+            img_coord_1 = self.camera_model_velo_camera_0.projectToImage([v.xPos, v.yPos, v.zPos])
+            img_coords.append((img_coord_0, img_coord_1))
 
-    def save_cars(self,img,list):
-
-        for pos in list:
-            scale=(pos[2]/15.0)
-            if scale<1:
-                scale=1
-            pos[0],pos[1]=self.checkCoord(img,pos)
-            im=img[int(pos[1]-(64/scale)):int(pos[1]),int(pos[0]):int(pos[0]+(64/scale))]
-            im=cv2.resize(im,(64,64))
-            cv2.imwrite('vehicles/'+str(self.drive_num)+'vehicle'+str(self.car_count)+'.png',im)
-
-            self.car_count+=1
-        self.save_non_cars(img,list)
+        return img_coords
 
     def showVisuals(self, path,date):
         fig=plt.figure()
@@ -87,43 +61,77 @@ class GroundtruthVisualizer:
         vehiclePositions = VehiclePositions(path,date, self.drive_num)
 
         syncFolder = "{0}_drive_{1}_sync".format(date, self.drive_num)
-        imgFolder = "image_{}".format('02') # always use camera 2 for colored images
-        imagePath = os.path.join(path, date, syncFolder, date, syncFolder, imgFolder,'data')
-        img_glob = os.path.join(imagePath, "*.png")
+        imgFolder = os.path.join(path, date, syncFolder, date, syncFolder, "image_{}",'data')
+        cam0_imgPath = imgFolder.format('00')
+        cam1_imgPath = imgFolder.format('01')
+        images_camera_0 = map(lambda img: os.path.join(cam0_imgPath, img), os.listdir(cam0_imgPath))
+        images_camera_1 = map(lambda img: os.path.join(cam1_imgPath, img), os.listdir(cam1_imgPath))
 
-        # load all images
-        loaded_images = [matplotlib.image.imread(img) for img in glob.glob(img_glob)]
+        # check that for every frame of one camera there is an image from the other
+        assert(len(images_camera_0) == len(images_camera_1))
 
-        for img in loaded_images:
+        # load all images:
+        sys.stdout.write("Loading images...")
+        start_time = time.time()
+        loaded_images = []
+        for i, (pic0, pic1) in enumerate(zip(images_camera_0, images_camera_1)):
+
+            loaded_pic_0 = cv2.imread(pic0, cv2.IMREAD_UNCHANGED)
+            loaded_pic_1 = cv2.imread(pic1, cv2.IMREAD_UNCHANGED)
+            vehicles = self.getRealVehiclePositions(i, vehiclePositions)
+
+            loaded_images.append(StereoVisionImage(loaded_pic_0, loaded_pic_1, vehicles))
+
+            # convert RGB between BGR : cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        sys.stdout.write("{}s\n".format(time.time() - start_time))
+
+        sys.stdout.write("Searching for Vehicles in {} images...".format(len(loaded_images)))
+        start_time = time.time()
+        # TODO: implement
+        sys.stdout.write("{}s\n".format(time.time() - start_time))
+
+
+        print("Show Range estimations...\n")
+
+        for stereo_vision_image in loaded_images:
+            img0 = stereo_vision_image.image0
+            img1 = stereo_vision_image.image1
+            real_vehicle_positions = stereo_vision_image.real_vehicle_positions
+
             if not plt.get_fignums():
                 # window has been closed
                 return
 
             #print ('new Image:')
             ax1=fig.add_subplot(211)
-            ax1.imshow(img,cmap='gray')
+            ax1.imshow(img0,cmap='gray')
 
 
             ax2=fig.add_subplot(212)
-            car_list=[]
-            vehicles=vehiclePositions.getVehiclePosition(i)
-            count=len(vehicles)
-            for j in range(count):
-                v = vehicles[j]
-                name = v.type
-                color=self.getVehicleColor(name)
 
-                ax2.add_patch(patches.Rectangle((- v.yPos + v.width , v.xPos - v.length),v.width ,v.length ,angle=v.angle ,color=color) )
-                vehicleCoord=[v.xPos ,v.yPos ,v.zPos]
-                image_coords = self.camera_model.projectToImage(vehicleCoord)
-                ax1.add_patch(patches.Rectangle(image_coords,2,2,color=color))
-                if name=='Car':
-                    dist=math.sqrt(vehicleCoord[0]**2+vehicleCoord[1]**2)
-                    image_coords.append(dist)
-                    car_list.append(image_coords)
+            # TODO: use vehicle positions from image analysis
+            count = len(real_vehicle_positions)
+            for j in range(count):
+                v = real_vehicle_positions[j]
+                name = v.type
+                color = self.getVehicleColor(name)
+
+                # show car in top-view coord system
+                ax2.add_patch(
+                    patches.Rectangle((- v.yPos + v.width, v.xPos - v.length), v.width, v.length, angle=v.angle,
+                                      color=color))
+
+                # show vehicle position in image
+                vehicleCoord = [v.xPos, v.yPos, v.zPos]
+                distance = util.distance(self.camera_model_1.getCameraPosition(), vehicleCoord)
+                image_coords = self.camera_model_velo_camera_0.projectToImage(vehicleCoord)
+                patch = patches.Rectangle(image_coords, 20, 20, color=color)
+                ax1.add_patch(patch)
+                # add distance description
+                ax1.text(image_coords[0], image_coords[1]+40, "{}m".format(distance), color=color)
+
+
             #fig.draw
-            if i%6==0:
-                self.save_cars(img,car_list)
             ax2.set_ylim([0,100])
             ax2.set_xlim([-25,25])
             ax2.set_aspect(1)
@@ -133,3 +141,14 @@ class GroundtruthVisualizer:
             #time.sleep(10)
             i+=1
 
+class StereoVisionImage:
+    def __init__(self, imageWithVehicles0, imageWithVehicles1, real_vehicle_positions):
+        self.image0 = imageWithVehicles0
+        self.image1 = imageWithVehicles1
+        self.real_vehicle_positions = real_vehicle_positions
+        self.vehicles0 = None
+        self.vehicles1 = None
+
+    def setVehicles(self, vehicles0, vehicles1):
+        self.vehicles0 = vehicles0
+        self.vehicles1 = vehicles1
