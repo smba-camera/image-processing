8104276696a982b0aa1d57df10de4f31b12fdcda
Daniel <merzdanieldeveloper@gmail.com>
2017-09-21 13:00:12 +0200

    vehicle detection serialization: load and save

--- image_processing/vehicle_detection/detect_vehicles_serialize.py
+++ image_processing/vehicle_detection/detect_vehicles_serialize.py
@@ -4,32 +4,45 @@ import glob
 import os
 import sys
 import pickle
-
-# make modules accessible for the script
-sys.path.append(os.path.abspath(os.path.join(".")))
-
-import image_processing.vehicle_detection.Vehicle_detection as Vehicle_detection
+from datetime import datetime
+from Vehicle_detection import VehicleDetection
+#import image_processing.vehicle_detection.VehicleDetection as VehicleDetection
 
 def detect_vehicles_and_save(path, file_name):
+    start_time = datetime.now()
     images = []
-    for i in glob(os.path.join(path, '*.png')):
+    for i in glob.glob(os.path.join(path, '*.png')):
         images.append(cv2.imread(i))
-    vehicle_detector = Vehicle_detection(images[0])
+    vehicle_detector = VehicleDetection(images[0])
     vehicles = []
+
+    counter = 1
+    #images = images[:10]
+    print("amount of images: {}, takes about {}m".format(len(images), len(images)/0.75))
     for i in images:
         vehicles.append(vehicle_detector.find_vehicles(i))
-    file_path = os.path.join('data', 'detected_vehicles', file_name)
+        now = datetime.now()
+        print("{}/{} time remaining: {}".format(counter,len(images), (now-start_time)/counter*(len(images)-counter)))
+        counter += 1
 
-    with open(file_path,'w') as f:
+    path = os.path.join('data', 'detected_vehicles')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file_path = os.path.join(path, file_name)
+
+    with open(file_path,'wb') as f:
         pickle.dump(vehicles, f)
 
 def load_detected_vehicles(file_name):
     file_path = os.path.join('data', 'detected_vehicles', file_name)
-    with open(file_path, 'r') as f:
+    with open(file_path, 'rb') as f:
         return pickle.load(f)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Renders Kitti data with marked positions of objects')
-    parser.add_argument('images_path', type=int)
-    parser.add_argument('file_name', type=int)
+    parser.add_argument('images_path')
+    parser.add_argument('file_name')
+    #parser.add_argument('path')
+    args = parser.parse_args()
+    detect_vehicles_and_save(args.images_path, args.file_name)
+    #print(load_detected_vehicles(args.path))
