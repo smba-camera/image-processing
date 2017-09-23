3426b8ee6737d6158cce551be3b017d1937163a4
Angloher <danielangloher@yahoo.de>
2017-09-23 03:35:46 -0500

    update plot threshold influence

--- scripts/plot_threshold_influence.py
+++ scripts/plot_threshold_influence.py
@@ -12,8 +12,8 @@ import image_processing.vehicle_detection.vehicle_detection_analyzer as compare
 
 def plot_threshold_influence():
     date = '2011_09_26'
-    drives = ['0056']
-    thresholds=['100','160','180','200','220']
+    drives = ['0022','0056']
+    thresholds=['100','160','200','240']#['100','160','180','200','220']
     startFrame = 0
     maxFrame = 100
     alpha = 10
@@ -28,15 +28,18 @@ def plot_threshold_influence():
             datapath_left = drive + '_03_t'+thresh
             datapath_right = drive + '_02_t'+thresh
             matcher.runComparison(date, drive, datapath_left, datapath_right, alpha)
-            print matcher.matchedCars
-            falseNegatives[i]=matcher.error_rate
-            falsePositives[i]=matcher.num_false_positives
+            falseNegatives[i]+=matcher.num_false_negatives
+            print falseNegatives
+            falsePositives[i]+=matcher.num_false_positives
            # print falseNegatives,falsePositives
             matcher.reset()
             i += 1
-
+    aggregated_errors=[sum(x) for x in zip(*[falsePositives,falseNegatives])]
+    thresholds=[int(x)/255.0 for x in thresholds ]
     plt.plot(thresholds, falseNegatives, 'bo-',label='false negatives',color='green')
     plt.plot(thresholds, falsePositives, 'bo-',label='false positives',color='blue')
+    plt.plot(thresholds, aggregated_errors, 'bo-', label='false positives', color='red')
+
     plt.legend(loc='best')
     #plt.axis([np.min(thresholds)-10, np.max(thresholds)+10, max(np.max(falsePositives),np.max(falseNegatives))+1])
     #plt.ylabel('Average deviation from correct position per meter distance [m]')
